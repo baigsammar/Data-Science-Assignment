68133a02b0fea1f6918fc3d0531c84e8c9dd5046
Ammar Baig <baigsammar@gmail.com>
2022-01-01 16:58:25 +0500

    Work Complete

---
+++ Assignment 1/assig 1.py
@@ -0,0 +1,24 @@
+print ("Twinkle, twinkle, little star")
+print("          How I wonder what you are")
+print("                 Up above the world so high")
+print("                 Like a diamond in the sky")
+print("Twinkle, twinkle, little star")
+print("         How I wonder what you are")
+
+import sys
+print("Python version is ", sys.version)
+
+import datetime
+print("Current datetime is ", datetime.datetime.now())
+
+radius=float(input("Enter Radius : "))
+pi=3.14
+print("Area of Cirle is ", radius*pi )
+
+firstname=input("Enter your First Name : ")
+lastname=input("Enter your Last Name : ")
+print(lastname," ",firstname)
+
+x=int(input("Number 1 : "))
+y=int(input("Number 2 : "))
+print(x+y)
